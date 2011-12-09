0aac3d5ac8a5d021af3f1b58f28513423d065349
Anshik <aanshiik@gmail.com>
2011-12-09 10:45:38 +0400

    added django.po for ru and added strings for localization in models.py

--- cmsplugin_filer_gallery/models.py
+++ cmsplugin_filer_gallery/models.py
@@ -11,10 +11,10 @@ CAPTION_ANIMATION_CHOICES=tuple(enumerate(CAPTION_ANIMATION_CHOICES))
 class FilerGallery(CMSPlugin):
 
     gallery = models.ForeignKey('filer_gallery.Gallery')
-    height = models.SmallIntegerField(default=200)
-    width = models.SmallIntegerField(default=300)
-    thumb_height = models.SmallIntegerField(null=True, blank=True, default=None, help_text='Leave empty for no thumbs.')
-    thumb_width = models.SmallIntegerField(null=True, blank=True, default=None, help_text='Leave empty for no thumbs.')
+    height = models.SmallIntegerField(_("height"),default=200)
+    width = models.SmallIntegerField(_("width"), default=300)
+    thumb_height = models.SmallIntegerField(null=True, blank=True, default=None, help_text=_('Leave empty for no thumbs.'))
+    thumb_width = models.SmallIntegerField(null=True, blank=True, default=None, help_text=_('Leave empty for no thumbs.'))
     
     class Meta:
         verbose_name = _("django filer gallery")
